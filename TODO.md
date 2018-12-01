ce804c5d94ae615db4d9300f8b160360949a422b
megavexus <nexus.megavexus@gmail.com>
2018-12-01 11:10:31 +0100

    Pequeña refractorizacion de nombres

---
+++ TODO.md
@@ -0,0 +1,9 @@
+* [x] Añadir paginación
+* [ ] Hacer endpoint de devices
+* [ ] Hacer endpoint de users
+* [ ] Hacer endpoint de modificacion de usuarios (status)
+* [ ] Hacer endpoint de modificacion de dispositivos (status)
+* [ ] Hacer endpoint de alertas
+* [ ] Hacer endpoint de alertas con filtro de device, dominio y user
+* [ ] Hacer endpoint de detalle de alerta
+* [ ] Hacer endpoint de dominios
--- tests/webserver/test_users.py
+++ tests/webserver/test_users.py
@@ -7,10 +7,16 @@ def test_get_all_users():
     assert len(list_data) > 0
     assert type(list_data[0]["_source"]) == dict
 
+def test_get_all_users_limit():
+    user_manager = Users()
+    list_data = user_manager.get_list_users(max_count=1)
+    assert len(list_data) == 1
+    assert type(list_data[0]["_source"]) == dict
+
+
 def test_get_details_devices():
     user_manager = Users()
     user_data = user_manager.get_user_details("3")
-    raise Exception(user_data)
     assert type(user_data) == dict
     assert user_data["name"] == "Lucas Fernandez"
     assert 'alerts' in user_data
